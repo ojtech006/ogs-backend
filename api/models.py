b809f943471c2cf8e3ccda944329f01d53a3ebc4
ojtech006 <ojtech006@gmail.com>
2025-06-04 07:07:59 -0400

    Update models.py

--- api/models.py
+++ api/models.py
@@ -15,7 +15,7 @@ class ContactMessage(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    link = models.URLField(null=True, blank=True)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 
     def __str__(self):
